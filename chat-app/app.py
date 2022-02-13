860ec5582003d69be5cc23b4c4256a2716136de0
Chris Kronberg <ckronber@gmail.com>
2022-02-12 22:02:12 -0500

    port set to 80

--- chat-app/app.py
+++ chat-app/app.py
@@ -1,27 +1,25 @@
-from asyncio import sslproto
 from distutils.util import convert_path
-from ssl import CERT_REQUIRED
+from smtplib import SMTP_SSL_PORT
 from website import create_app
 from website.views import sio
-
 
 #This port is used when testing on a domain
 #PORT = 5000
 #PORT = 80
 #PORT = 443
+
 HOST = "0.0.0.0"
 #HOST = "localhost"
-#CERT = "C:\\Users\ckron\cert.pem"
-#KEY= "C:\\Users\ckron\key.pem"
-CERT = ".\\chat-app\cert.pem"
-KEY= ".\\chat-app\key.pem"
-
+#my_CERT = "C:\\Users\ckron\cert.pem"
+#my_KEY= "C:\\Users\ckron\key.pem"
+#CERT = ".\\chat-app\cert.pem"
+#KEY= ".\\chat-app\key.pem"
 app = create_app()
-sio.init_app(app,ssl_context = (CERT,KEY))
+sio.init_app(app)
 #client = socketio.test_client(app)
 
 if __name__ == "__main__":
-   # app.run(debug=True)
+    #app.run(sio,debug=True,ssl_context = (CERT,KEY))
     #sio.run(app,debug=True,port=PORT,host=HOST)
-    sio.run(app,debug=True,port=8000,host=HOST)
+    sio.run(app,debug=True,port=80,host=HOST)
     #sio.run(app,debug=True,host=HOST,port=PORT)
