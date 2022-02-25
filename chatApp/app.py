b9754ee3ddc0b6825f5948ebe1fa7c8edc2ab322
Chris Kronberg <ckronber@gmail.com>
2022-02-25 00:55:49 -0500

    changed port to 80

--- chatApp/app.py
+++ chatApp/app.py
@@ -3,15 +3,15 @@ from website import create_app
 from website.views import sio
 app = create_app()
 sio.init_app(app)
-##PORT = 80
+PORT = 80
 #PORT = 443
-PORT = 5000
+#PORT = 5000
 HOST = "localhost"
 #MYCERT = "C:\\Users\\ckron\\cert.pem"
 #MYKEY = "C:\\Users\\ckron\\key.pem"
 
 if __name__ == "__main__":
-    sio.run(app,debug=True,host=HOST,port=PORT,cors_allowed_origins="*")
+    sio.run(app,debug=True,host=HOST,port=PORT)
     #sio.run(app,port=PORT,host=HOST,certfile=MYCERT,keyfile=MYKEY,server_side=True,debug=True)
     #sio.run(app,debug=True,port=80,host=HOST)
     #sio.run(app,debug=True,host=HOST,port=PORT
