940e24568fced38862f1bda61a3a659166ce7818
Chris Kronberg <ckronber@gmail.com>
2022-03-17 17:49:51 -0400

    changed some database stuff

--- website/__init__.py
+++ website/__init__.py
@@ -27,10 +27,9 @@ def create_app():
     #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
    
     db.init_app(app)
-    migrate = Migrate(app=app,db=db)
+    #migrate = Migrate(app=app,db=db)
     
-    if db.__sizeof__() > 0:
-        db.create_all(app=app)
+    db.create_all(app=app)
     #if path.isfile(FILEPATH+DB_NAME) is not True:
     #    db.create_all(app=app)
     #    print("created")
