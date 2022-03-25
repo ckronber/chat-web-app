7e0d6ef451f778486a91c0b3f9e567f2aa5ec0cb
Chris Kronberg <ckronber@gmail.com>
2022-03-24 22:42:26 -0400

    updated to not print out db_online bool variable

--- website/__init__.py
+++ website/__init__.py
@@ -13,10 +13,10 @@ def create_app():
     app = Flask(__name__)
     uri = environ.get("DATABASE_URL")
     db_online = environ.get("db-online")
-    print("DB_ONLINE = " +db_online)
+
     if db_online == None:
         db_online = False
-
+    
     if uri and uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
         if(path.isfile(FILEPATH+DB_NAME)):
@@ -25,11 +25,6 @@ def create_app():
     app.config['SECRET_KEY'] = 'mySecretKey'
     app.config['SQLALCHEMY_DATABASE_URI'] = uri or f'sqlite:///{DB_NAME}' 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    #app = Flask(__name__)
-    #app.config['SECRET_KEY'] = 'mySecretKey'
-    #app.config['SQLALCHEMY_DATABASE_URI'] =  HEROKU_URI
-    #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
    
     db.init_app(app)
     #migrate = Migrate(app=app,db=db)
