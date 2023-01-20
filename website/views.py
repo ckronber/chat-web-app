a08c7301f35e61706174ec5ab6ab7a0836bc1dc3
Chris Kronberg <ckronber@gmail.com>
2023-01-20 16:05:17 -0600

    Update views.py

--- website/views.py
+++ website/views.py
@@ -29,16 +29,14 @@ thread_lock = Lock()
 def home():
     myNotes = Note.query.all()
     users = User.query.all()
-    #,async_mode = sio.async_mode
-    return render_template('home.html',allNotes=myNotes,users = users,user=current_user)
+    return render_template('home.html',allNotes=myNotes,users = users,user=current_user,async_mode = sio.async_mode)
 
 #ROUTE FOR ACCOUNT WEBPAGE
 #========================================================================================
 @views.route('/account',methods=['GET','POST']) 
 @login_required
 def account():
-    #async_mode = sio.async_mode
-    return render_template('userSettings.html',user = current_user)
+    return render_template('userSettings.html',user = current_user,async_mode = sio.async_mode)
 
 
 #ONLINE/OFFLINE
