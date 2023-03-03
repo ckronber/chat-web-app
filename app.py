270068bbfcd93ce8c2d24bc53265b5021d8df9b2
Chris Kronberg <ckronber@gmail.com>
2023-03-03 15:10:25 -0600

    removed monkey.patch_all() from code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from gevent import monkey
-monkey.patch_all()
+#from gevent import monkey
+#monkey.patch_all()
 from website import create_app
 from website.views import sio
 from gevent.pywsgi import WSGIServer
