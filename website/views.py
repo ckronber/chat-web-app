f1c0d24322294a47240c128167e6d82ad7fec970
Chris Kronberg <ckronber@gmail.com>
2023-03-03 08:32:22 -0600

    Update views.py

--- website/views.py
+++ website/views.py
@@ -10,12 +10,12 @@ from . import db
 
 database_online = os.environ.get("db-online")
 
-if  database_online == "True":
-    async_mode = "greenlet"
-else:
-    async_mode = "eventlet"
+#if  database_online == "True":
+#    async_mode = "greenlet"
+#else:
+#    async_mode = "eventlet"
     
-async_mode = None
+async_mode = "eventlet"
     
 sio = SocketIO()
 sio.asyc_mode = async_mode
