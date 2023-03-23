85b22ad36e0b9fb6b031ce5cb1d24fc882631fe2
Chris Kronberg <ckronber@gmail.com>
2023-03-23 07:48:59 -0500

    added cors and removed some unnecessary code

--- app.py
+++ app.py
@@ -1,26 +1,23 @@
-#from gevent import monkey
-#monkey.patch_all()
 from website import create_app
 from website.views import sio
-from gevent.pywsgi import WSGIServer
 import os
-
-#added to the app and is ready for push
 
 app = create_app()
 database_online = os.environ.get("db-online")
 sio.init_app(app)
 
+#* Here are the ports that are used
 PORTS = [443,5000]
 PORT = PORTS[1]
 HOST = "localhost"
 SSLCERT = ["C:\\Users\\ckron\\cert.pem", "C:\\Users\\ckron\\key.pem"]
 
+#* This is the main function to start the server
 if __name__ == "__main__":
     if(database_online == "True"):
-        app.run()
+        app.run(cors_allowed_origins='*')
     else:
         if PORT == 443:
-            sio.run(app,debug=True,port=PORT,host=HOST,certfile=SSLCERT[0],keyfile=SSLCERT[1])
+            sio.run(app,debug=True,port=PORT,host=HOST,certfile=SSLCERT[0],keyfile=SSLCERT[1],server_side=True)
         else:
             sio.run(app,debug=True,host=HOST,port=PORT)
